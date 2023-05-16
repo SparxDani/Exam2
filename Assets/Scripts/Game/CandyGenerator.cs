d680b02696f174bac4d103cc8c23317f2770c371
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-05-16 12:17:47 -0500

    Update CandyGenerator.cs

--- Assets/Scripts/Game/CandyGenerator.cs
+++ Assets/Scripts/Game/CandyGenerator.cs
@@ -55,20 +55,7 @@ public class CandyGenerator : MonoBehaviour
             Destroy(candy_script.gameObject);
             return;
         }
-        if (candy_script.frame == 3)
-        {
-            SceneManager.LoadScene("GameOver");
-            return;
-        }
-        int lives = player_script.player_lives;
-        int live_changer = candy_script.lifeChanges;
-        lives += live_changer;
-        print(lives);
-        if (lives <= 0)
-        {
-            SceneManager.LoadScene("GameOver");
-        }
-        player_script.player_lives = lives;
+        
         Destroy(candy_script.gameObject);
     }
 
